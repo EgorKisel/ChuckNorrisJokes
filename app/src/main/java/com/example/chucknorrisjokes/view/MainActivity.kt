1862f4d01c88ab2886c33878184813184b6ad771
Egor Kisel <kisel1989@mail.ru>
2023-02-02 08:28:52 +0300

    application layout

--- app/src/main/java/com/example/chucknorrisjokes/view/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisjokes/view/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.example.chucknorrisjokes
+package com.example.chucknorrisjokes.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chucknorrisjokes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance()).commit()
+        }
     }
 }
